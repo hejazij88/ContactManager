8b23cd5d7a28857a2e29fe40c516d3210e29c746
98913 <98913@LAPTOP-JQR66TAF>
2025-04-18 19:12:29 +0330

    Add Contact Infrustctuare

---
+++ Contact.Infrus/ContactContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contact.Infrus.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contact.Infrus
+{
+    public class ContactContext:DbContext
+    {
+
+        public ContactContext(DbContextOptions<ContactContext> option):base(option)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(ContactMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Contact.Infrus/Mapping/ContactMapping.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Contact.Infrus.Mapping;
+
+public class ContactMapping:IEntityTypeConfiguration<Domain.Contact>
+{
+    public void Configure(EntityTypeBuilder<Domain.Contact> builder)
+    {
+        builder.ToTable("Contact");
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name).IsRequired();
+        builder.Property(c => c.LastName).IsRequired();
+        builder.Property(c => c.Phone).IsRequired();
+
+
+    }
+}
