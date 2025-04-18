635179ccfce84e8a0f28e25f89458c4fbc22668e
98913 <98913@LAPTOP-JQR66TAF>
2025-04-18 18:47:25 +0330

    Add Header Component

---
+++ ContactManager/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.ViewComponents;
+
+public class HeaderViewComponent:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
